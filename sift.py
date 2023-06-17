61f4068b096fedc90e0d691d754a7acede193bb3
Midnaaaaaa <ijuaagi@gmail.com>
2023-06-18 00:35:10 +0200

    ya van algunas cosas, se podria entregar :thumbsup:

--- sift.py
+++ sift.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 import os
 
-def sift_tracking():
-    BB = np.genfromtxt('MotorcycleChase/groundtruth_rect.txt', delimiter=',', dtype=int)
+def sift_tracking(video):
+    BB = np.genfromtxt(video + '/groundtruth_rect.txt', delimiter=',', dtype=int)
 
-    base_path = 'MotorcycleChase/img/'
+    base_path = video+'/img/'
     scale = 1
-    Idir = os.listdir('MotorcycleChase/img/')  # Directorio que contiene las imágenes
+    Idir = os.listdir(base_path)  # Directorio que contiene las imágenes
     nf = len(Idir)  # Número total de imágenes
     i = 0
     filename = Idir[i]  # Nombre del archivo de imagen
@@ -18,10 +18,8 @@ def sift_tracking():
     IC = I[int(rect[1]):int(rect[1] + rect[3]), int(rect[0]):int(rect[0] + rect[2])]  # Recortar la imagen
 
     IQ = cv2.rectangle(I, (int(rect[0]), int(rect[1])), (int(rect[0] + rect[2]), int(rect[1] + rect[3])), (0, 255, 0), 2)  # Dibujar el rectángulo de interés en la imagen
-    cv2.imshow('Image', IC)
-    cv2.waitKey(0)
 
-    for i in range(1, 50):
+    for i in range(1, len(Idir)):
         im_obj = cv2.cvtColor(IC, cv2.COLOR_BGR2GRAY)  # Convertir la imagen a escala de grises
         filename = Idir[i]
         I2 = cv2.resize(cv2.imread(base_path + filename), None, fx=scale, fy=scale)
@@ -34,29 +32,41 @@ def sift_tracking():
 
         bf = cv2.BFMatcher()  # Crear el objeto correspondiente
         matches = bf.match(feat_obj, feat_esc)  # Encontrar los puntos coincidentes más cercanos
-        matches = sorted(matches, key=lambda x: x.distance)
+        matches = sorted(matches, key = lambda x:x.distance)
 
         # Filter good matches based on a distance threshold
         good_matches = [m for m in matches if m.distance < 150]
 
+        #good_matches = matches[00:50]
+
+        #im3 = (cv2.drawMatches(im_obj, kp_obj, im_esc, kp_esc, good_matches, im_esc))
+        #cv2.imshow('SIFT', im3)
+        #cv2.waitKey(0)
+
         # Find transformation between matched keypoints
-        src_pts = np.float32([kp_obj[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp_esc[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        src_points = np.float32([kp_obj[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        dst_points = np.float32([kp_esc[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
-        M, _ = cv2.estimateAffinePartial2D(src_pts, dst_pts)
 
-        # Transform the bounding box from the previous frame
-        rect_pts = np.float32([[rect[0], rect[1]], [rect[0] + rect[2], rect[1]], [rect[0], rect[1] + rect[3]], [rect[0] + rect[2], rect[1] + rect[3]]]).reshape(-1, 1, 2)
-        transformed_pts = cv2.transform(rect_pts, M)
+        M, _ = cv2.findHomography(src_points, dst_points, cv2.RANSAC, 5.0)
 
-        # Update the rect values based on the transformed bounding box
-        rect = (min(transformed_pts[:, 0, 0]), min(transformed_pts[:, 0, 1]), max(transformed_pts[:, 0, 0]) - min(transformed_pts[:, 0, 0]), max(transformed_pts[:, 0, 1]) - min(transformed_pts[:, 0, 1]))
+        pts = np.float32([[0, 0], [rect[2], 0], [rect[2], rect[3]], [0, rect[3]]]).reshape(-1, 1, 2)
+        transformed_pts = cv2.perspectiveTransform(pts, M)
+        x_min_new = int(np.min(transformed_pts[:, 0, 0]))
+        y_min_new = int(np.min(transformed_pts[:, 0, 1]))
+        x_max_new = int(np.max(transformed_pts[:, 0, 0]))
+        y_max_new = int(np.max(transformed_pts[:, 0, 1]))
 
-        # Draw the transformed bounding box on the current frame using cv2.rectangle
-        I2 = cv2.rectangle(I2, (int(rect[0]), int(rect[1])), (int(rect[0] + rect[2]), int(rect[1] + rect[3])), (0, 255, 0), 2)
+
+        IC = I2[int(y_min_new):int(y_max_new), int(x_min_new):int(x_max_new)]  # Recortar la imagen
+
+        IQ = cv2.rectangle(I2, (x_min_new, y_min_new), (x_max_new, y_max_new), (0, 255, 0), 2);
 
         #cv2.imshow('SIFT', im3)
-        cv2.imshow('Bounding Box', I2)
-        cv2.waitKey(0)
+        cv2.imshow('Bounding Box', IQ)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-sift_tracking()
+
+
+sift_tracking('MotorcycleChase')
